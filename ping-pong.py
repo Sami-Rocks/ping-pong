8ed1990133662b91881b63d2ba87cf7a912e007a
Sami <sowusuaboagye@gmail.com>
2019-10-07 11:04:47 +0100

    done

--- ping-pong.py
+++ ping-pong.py
@@ -1,4 +1,5 @@
 import turtle
+import winsound
 
 win = turtle.Screen()
 win.title("My Game")
@@ -26,6 +27,22 @@ paddle_b = paddle(350)
 ball = paddle(0, 1)
 ball.dx = 0.2
 ball.dy = 0.2
+
+
+score_a = 0
+score_b = 0
+
+
+pen = turtle.Turtle()
+pen.speed(0)
+pen.color('white')
+pen.penup()
+pen.hideturtle()
+pen.goto(0, 260)
+pen.write("Player A: 0  Player B: 0", align="center", font=("Courier", 24, "normal"))
+
+
+
 def paddle_a_move_up():
     y = paddle_a.ycor()
     y+=20
@@ -61,22 +78,31 @@ while True:
     if ball.ycor()>290:
         ball.sety(290)
         ball.dy *= -1
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
+
     if ball.ycor()< -290:
         ball.sety(-290)
         ball.dy *= -1
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
 
     if ball.xcor() > 390:
         ball.goto(0,0)
         ball.dx *= -1
+        score_a += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
     if ball.xcor() < -390:
         ball.goto(0,0)
         ball.dx *= -1
-    
-
+        score_b += 1
+        pen.clear()
+        pen.write("Player A: {}  Player B: {}".format(score_a, score_b), align="center", font=("Courier", 24, "normal"))
     if (ball.xcor() > 340 and ball.xcor() < 350) and (ball.ycor() < paddle_b.ycor() + 40 and ball.ycor() > paddle_b.ycor() - 40):
         ball.setx(340)
         ball.dx *= -1
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
 
     if (ball.xcor() < -340 and ball.xcor() > -350) and (ball.ycor() < paddle_a.ycor() + 40 and ball.ycor() > paddle_a.ycor() - 40):
         ball.setx(-340)
         ball.dx *= -1
+        winsound.PlaySound("bounce.wav", winsound.SND_ASYNC)
